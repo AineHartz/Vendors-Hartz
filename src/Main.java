b251890dab8dc1ddfa0abbebe270d60a8d4dad19
AineHartz <ryanhartz9169@gmail.com>
2024-11-07 10:02:57 -0600

    Added a test case and functionality to add money to vendor. Pending if it works or not, we'll see.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Ultra Shop!");
-
-
-
         System.out.println("Maintained by <A human>");
 
     }
--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -4,9 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendorTest {
 
+    @BeforeEach
+    void setup()
+    {
+        Vendor vendor = new Vendor(2, 5);
+    }
+
     @Test
     void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void addMoney(int amount)
+    {
+        assertEquals(vendor.getBalance+amount, vendor.addMoney(amount))
     }
 
 }
